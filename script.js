eae3f82514effb84ce47894ff6cb3ba6423690bf
Arthur622352 <arthurviniciussramos@gmail.com>
2025-10-05 19:09:54 -0300

    oi

---
+++ script.js
@@ -0,0 +1,9 @@
+ <script>
+    function tocarMusica() {
+      const audio = document.getElementById('som')
+      const msg = document.getElementById('mensagem')
+
+      audio.play()
+      msg.style.display = 'block'
+    }
+  </script>
